fd79ab16bf915a2e4c607f02d39383b954847146
testtimshaq <58642417+testtimshaq@users.noreply.github.com>
2021-01-06 06:20:50 +0500

    desktop ver

--- build/js/script.js
+++ build/js/script.js
@@ -2,305 +2,70 @@
 
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-/******/
-(function (modules) {
-  // webpackBootstrap
+!function (e) {
+  var t = {};
 
-  /******/
-  // The module cache
-
-  /******/
-  var installedModules = {};
-  /******/
-
-  /******/
-  // The require function
-
-  /******/
-
-  function __webpack_require__(moduleId) {
-    /******/
-
-    /******/
-    // Check if module is in cache
-
-    /******/
-    if (installedModules[moduleId]) {
-      /******/
-      return installedModules[moduleId].exports;
-      /******/
-    }
-    /******/
-    // Create a new module (and put it into the cache)
-
-    /******/
-
-
-    var module = installedModules[moduleId] = {
-      /******/
-      i: moduleId,
-
-      /******/
-      l: false,
-
-      /******/
+  function n(r) {
+    if (t[r]) return t[r].exports;
+    var o = t[r] = {
+      i: r,
+      l: !1,
       exports: {}
-      /******/
-
     };
-    /******/
-
-    /******/
-    // Execute the module function
-
-    /******/
-
-    modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-    /******/
-
-    /******/
-    // Flag the module as loaded
-
-    /******/
-
-    module.l = true;
-    /******/
-
-    /******/
-    // Return the exports of the module
-
-    /******/
-
-    return module.exports;
-    /******/
+    return e[r].call(o.exports, o, o.exports, n), o.l = !0, o.exports;
   }
-  /******/
 
-  /******/
-
-  /******/
-  // expose the modules object (__webpack_modules__)
-
-  /******/
-
-
-  __webpack_require__.m = modules;
-  /******/
-
-  /******/
-  // expose the module cache
-
-  /******/
-
-  __webpack_require__.c = installedModules;
-  /******/
-
-  /******/
-  // define getter function for harmony exports
-
-  /******/
-
-  __webpack_require__.d = function (exports, name, getter) {
-    /******/
-    if (!__webpack_require__.o(exports, name)) {
-      /******/
-      Object.defineProperty(exports, name, {
-        enumerable: true,
-        get: getter
-      });
-      /******/
-    }
-    /******/
-
-  };
-  /******/
-
-  /******/
-  // define __esModule on exports
-
-  /******/
-
-
-  __webpack_require__.r = function (exports) {
-    /******/
-    if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-      /******/
-      Object.defineProperty(exports, Symbol.toStringTag, {
-        value: 'Module'
-      });
-      /******/
-    }
-    /******/
-
-
-    Object.defineProperty(exports, '__esModule', {
-      value: true
+  n.m = e, n.c = t, n.d = function (e, t, r) {
+    n.o(e, t) || Object.defineProperty(e, t, {
+      enumerable: !0,
+      get: r
     });
-    /******/
-  };
-  /******/
-
-  /******/
-  // create a fake namespace object
-
-  /******/
-  // mode & 1: value is a module id, require it
-
-  /******/
-  // mode & 2: merge all properties of value into the ns
-
-  /******/
-  // mode & 4: return value when already ns object
-
-  /******/
-  // mode & 8|1: behave like require
-
-  /******/
-
-
-  __webpack_require__.t = function (value, mode) {
-    /******/
-    if (mode & 1) value = __webpack_require__(value);
-    /******/
-
-    if (mode & 8) return value;
-    /******/
-
-    if (mode & 4 && _typeof(value) === 'object' && value && value.__esModule) return value;
-    /******/
-
-    var ns = Object.create(null);
-    /******/
-
-    __webpack_require__.r(ns);
-    /******/
-
-
-    Object.defineProperty(ns, 'default', {
-      enumerable: true,
-      value: value
+  }, n.r = function (e) {
+    "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+      value: "Module"
+    }), Object.defineProperty(e, "__esModule", {
+      value: !0
     });
-    /******/
-
-    if (mode & 2 && typeof value != 'string') for (var key in value) {
-      __webpack_require__.d(ns, key, function (key) {
-        return value[key];
-      }.bind(null, key));
+  }, n.t = function (e, t) {
+    if (1 & t && (e = n(e)), 8 & t) return e;
+    if (4 & t && "object" == _typeof(e) && e && e.__esModule) return e;
+    var r = Object.create(null);
+    if (n.r(r), Object.defineProperty(r, "default", {
+      enumerable: !0,
+      value: e
+    }), 2 & t && "string" != typeof e) for (var o in e) {
+      n.d(r, o, function (t) {
+        return e[t];
+      }.bind(null, o));
     }
-    /******/
-
-    return ns;
-    /******/
-  };
-  /******/
-
-  /******/
-  // getDefaultExport function for compatibility with non-harmony modules
-
-  /******/
-
-
-  __webpack_require__.n = function (module) {
-    /******/
-    var getter = module && module.__esModule ?
-    /******/
-    function getDefault() {
-      return module['default'];
-    } :
-    /******/
-    function getModuleExports() {
-      return module;
+    return r;
+  }, n.n = function (e) {
+    var t = e && e.__esModule ? function () {
+      return e["default"];
+    } : function () {
+      return e;
     };
-    /******/
+    return n.d(t, "a", t), t;
+  }, n.o = function (e, t) {
+    return Object.prototype.hasOwnProperty.call(e, t);
+  }, n.p = "", n(n.s = 1);
+}([function (e, t, n) {
+  "use strict";
 
-    __webpack_require__.d(getter, 'a', getter);
-    /******/
-
-
-    return getter;
-    /******/
+  n.r(t), t["default"] = function () {
+    var e, t;
+    e = function e(_e) {
+      1 == _e ? document.querySelector("body").classList.add("webp") : document.querySelector("body").classList.add("no-webp");
+    }, (t = new Image()).onload = t.onerror = function () {
+      e(2 == t.height);
+    }, t.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
   };
-  /******/
+}, function (e, t, n) {
+  n(2), e.exports = n(0);
+}, function (e, t, n) {
+  "use strict";
 
-  /******/
-  // Object.prototype.hasOwnProperty.call
-
-  /******/
-
-
-  __webpack_require__.o = function (object, property) {
-    return Object.prototype.hasOwnProperty.call(object, property);
-  };
-  /******/
-
-  /******/
-  // __webpack_public_path__
-
-  /******/
-
-
-  __webpack_require__.p = "";
-  /******/
-
-  /******/
-
-  /******/
-  // Load entry module and return exports
-
-  /******/
-
-  return __webpack_require__(__webpack_require__.s = 0);
-  /******/
-})(
-/************************************************************************/
-
-/******/
-{
-  /***/
-  "./src/js/script.js":
-  /*!**************************!*\
-    !*** ./src/js/script.js ***!
-    \**************************/
-
-  /*! no exports provided */
-
-  /***/
-  function srcJsScriptJs(module, __webpack_exports__, __webpack_require__) {
-    "use strict";
-
-    eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _webpTest__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./webpTest */ \"./src/js/webpTest.js\");\n\r\n\r\n\r\n\r\nObject(_webpTest__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\r\n\r\ndocument.addEventListener(\"DOMContentLoaded\", function () {\r\n\r\n\r\n});\n\n//# sourceURL=webpack:///./src/js/script.js?");
-    /***/
-  },
-
-  /***/
-  "./src/js/webpTest.js":
-  /*!****************************!*\
-    !*** ./src/js/webpTest.js ***!
-    \****************************/
-
-  /*! exports provided: default */
-
-  /***/
-  function srcJsWebpTestJs(module, __webpack_exports__, __webpack_require__) {
-    "use strict";
-
-    eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function() {\r\n\r\nfunction testWebP(callback) {\r\n\r\n\tvar webP = new Image();\r\n\r\n\twebP.onload = webP.onerror = function () {\r\n\t\tcallback(webP.height == 2);\r\n\t};\r\n\r\n\twebP.src = \"data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA\";\r\n\r\n}\r\n\r\ntestWebP(function (support) {\r\n\r\n\tif (support == true) {\r\n\t\tdocument.querySelector('body')\r\n\t\t.classList.add('webp');\r\n\t} else {\r\n\t\tdocument.querySelector('body')\r\n\t\t.classList.add('no-webp');\r\n\t}\r\n\r\n});\r\n\r\n});\n\n//# sourceURL=webpack:///./src/js/webpTest.js?");
-    /***/
-  },
-
-  /***/
-  0:
-  /*!*****************************************************!*\
-    !*** multi ./src/js/script.js ./src/js/webpTest.js ***!
-    \*****************************************************/
-
-  /*! no static exports found */
-
-  /***/
-  function _(module, exports, __webpack_require__) {
-    eval("__webpack_require__(/*! C:\\Users\\timur\\Desktop\\alldeficoins\\src\\js\\script.js */\"./src/js/script.js\");\nmodule.exports = __webpack_require__(/*! C:\\Users\\timur\\Desktop\\alldeficoins\\src\\js\\webpTest.js */\"./src/js/webpTest.js\");\n\n\n//# sourceURL=webpack:///multi_./src/js/script.js_./src/js/webpTest.js?");
-    /***/
-  }
-  /******/
-
-});
+  n.r(t);
+  var r = n(0);
+  Object(r["default"])(), document.addEventListener("DOMContentLoaded", function () {});
+}]);
