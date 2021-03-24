fd7f6149c6c243996b6c93413dcf5ed907ab7663
testtimshaq <58642417+testtimshaq@users.noreply.github.com>
2021-03-24 15:21:32 +0500

    local del

--- build/js/script.js
+++ build/js/script.js
@@ -5,20 +5,20 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 !function (e) {
   var t = {};
 
-  function n(r) {
-    if (t[r]) return t[r].exports;
-    var o = t[r] = {
-      i: r,
+  function n(o) {
+    if (t[o]) return t[o].exports;
+    var r = t[o] = {
+      i: o,
       l: !1,
       exports: {}
     };
-    return e[r].call(o.exports, o, o.exports, n), o.l = !0, o.exports;
+    return e[o].call(r.exports, r, r.exports, n), r.l = !0, r.exports;
   }
 
-  n.m = e, n.c = t, n.d = function (e, t, r) {
+  n.m = e, n.c = t, n.d = function (e, t, o) {
     n.o(e, t) || Object.defineProperty(e, t, {
       enumerable: !0,
-      get: r
+      get: o
     });
   }, n.r = function (e) {
     "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
@@ -29,16 +29,16 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
   }, n.t = function (e, t) {
     if (1 & t && (e = n(e)), 8 & t) return e;
     if (4 & t && "object" == _typeof(e) && e && e.__esModule) return e;
-    var r = Object.create(null);
-    if (n.r(r), Object.defineProperty(r, "default", {
+    var o = Object.create(null);
+    if (n.r(o), Object.defineProperty(o, "default", {
       enumerable: !0,
       value: e
-    }), 2 & t && "string" != typeof e) for (var o in e) {
-      n.d(r, o, function (t) {
+    }), 2 & t && "string" != typeof e) for (var r in e) {
+      n.d(o, r, function (t) {
         return e[t];
-      }.bind(null, o));
+      }.bind(null, r));
     }
-    return r;
+    return o;
   }, n.n = function (e) {
     var t = e && e.__esModule ? function () {
       return e["default"];
@@ -48,8 +48,53 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
     return n.d(t, "a", t), t;
   }, n.o = function (e, t) {
     return Object.prototype.hasOwnProperty.call(e, t);
-  }, n.p = "", n(n.s = 1);
+  }, n.p = "", n(n.s = 0);
 }([function (e, t, n) {
+  n(1), e.exports = n(2);
+}, function (e, t, n) {
+  "use strict";
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var e = document.getElementById("header");
+
+    function t() {
+      window.scrollY >= 200 ? e.classList.add("active") : e.classList.remove("active");
+    }
+
+    function n() {
+      html.style.overflowX = "hidden", html.style.overflowY = "hidden";
+    }
+
+    function o() {
+      html.style.overflowX = "hidden", html.style.overflowY = "scroll";
+    }
+
+    t(), window.addEventListener("scroll", function (e) {
+      window.scrollY;
+      t();
+    });
+    var r = document.getElementById("burgerOpen"),
+        c = document.getElementById("burgerClose");
+    r.addEventListener("click", function () {
+      e.classList.add("open-menu"), n();
+    }), c.addEventListener("click", function () {
+      e.classList.remove("open-menu"), o();
+    });
+    var i = document.getElementById("searchOpen"),
+        l = document.getElementById("searchClose"),
+        d = document.getElementById("searchForm"),
+        u = d.querySelector("input");
+    i.addEventListener("click", function () {
+      e.classList.add("search"), n(), u.focus();
+    }), l.addEventListener("click", function () {
+      e.classList.remove("search"), o();
+    }), d.addEventListener("submit", function (e) {
+      e.preventDefault();
+      var t = u.value;
+      console.log(t), window.location.href = "/search?search=" + t;
+    });
+  });
+}, function (e, t, n) {
   "use strict";
 
   n.r(t), t["default"] = function () {
@@ -60,12 +105,4 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
       e(2 == t.height);
     }, t.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
   };
-}, function (e, t, n) {
-  n(2), e.exports = n(0);
-}, function (e, t, n) {
-  "use strict";
-
-  n.r(t);
-  var r = n(0);
-  Object(r["default"])(), document.addEventListener("DOMContentLoaded", function () {});
 }]);
--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,76 @@
 "use strict";
 
-import webpTest from './webpTest';
+// import webpTest from './webpTest';
 
-webpTest();
+// webpTest();
 
 document.addEventListener("DOMContentLoaded", function () {
 
+const header = document.getElementById('header');
+
+function headerActiveClass() {
+
+	if (window.scrollY >= 200) {
+		header.classList.add('active')
+	} else {
+		header.classList.remove('active')
+	}
+
+}
+
+headerActiveClass();
+
+window.addEventListener('scroll', function(ev) {
+	var scrollTop = window.scrollY
+	headerActiveClass()
+});
+
+function disableScroll() {
+	html.style.overflowX = 'hidden';
+	html.style.overflowY = 'hidden';
+};
+
+function enableScroll() {
+	html.style.overflowX = 'hidden';
+	html.style.overflowY = 'scroll';
+};
+
+// burger
+
+const burgerOpen = document.getElementById('burgerOpen');
+const burgerClose = document.getElementById('burgerClose');
+
+burgerOpen.addEventListener('click', function() {
+	header.classList.add('open-menu');
+	disableScroll();
+})
+
+burgerClose.addEventListener('click', function() {
+	header.classList.remove('open-menu');
+	enableScroll();
+})
+
+const searchOpen = document.getElementById('searchOpen');
+const searchClose = document.getElementById('searchClose');
+const searchForm = document.getElementById('searchForm');
+const searchInput = searchForm.querySelector('input');
+
+searchOpen.addEventListener('click', function() {
+	header.classList.add('search');
+	disableScroll();
+	searchInput.focus();
+})
+
+searchClose.addEventListener('click', function() {
+	header.classList.remove('search');
+	enableScroll();
+})
+
+searchForm.addEventListener('submit', function(ev) {
+	ev.preventDefault();
+	const inputValue = searchInput.value;
+	console.log(inputValue)
+	window.location.href = `/search?search=${inputValue}`;
+})
 
 });
